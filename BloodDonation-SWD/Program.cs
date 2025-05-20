0ad8a84c4468a3aa68cb5c9afff2337d46abd26a
Harry <thcstrandainghiathinh6a1@gmail.com>
2025-05-21 00:01:06 +0700

    Fix Program.cs for Render deploy

--- BloodDonation-SWD/Program.cs
+++ BloodDonation-SWD/Program.cs
@@ -1,22 +1,24 @@
-using Microsoft.EntityFrameworkCore;
-using BloodDonation_SWD.Controllers; // This line is likely still incorrect for DbContext namespace
-using BloodDonation_SWD.Models; // You'll likely need the models namespace here as well
+﻿using Microsoft.EntityFrameworkCore;
+using BloodDonation_SWD.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<BloodDonation_SWD.Models.BloodDonationContext>(options => // Assuming BloodDonationContext is in the Models namespace
+builder.Services.AddDbContext<BloodDonationContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// 👇 Thêm đoạn này để lấy PORT từ Render
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+builder.WebHost.UseUrls($"http://*:{port}");
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
